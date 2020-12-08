0329b3a7dd9ce00333d889b0f7593e0c234fc259
Salvador <hebustha@gmail.com>
2020-12-08 23:47:52 +0100

    Ad

--- assets/src/app.ts
+++ assets/src/app.ts
@@ -1,5 +1,7 @@
 import IconComponent from "App/components/icon.component";
 import {HomeComponent} from "App/modules/ui/components/home.template.components";
+import ProductComponent from "App/components/product.component";
 
 new HomeComponent();
 new IconComponent();
+new ProductComponent();
