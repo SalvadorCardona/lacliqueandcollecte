4d5b71b5114f0f28ca5f2a487bd02f1f894c2c8f
suleymanthr <suleymanthr@outlook.fr>
2021-04-14 17:02:10 +0200

    Fix - app home header

--- assets/src/components/home/home.header.component.ts
+++ assets/src/components/home/home.header.component.ts
@@ -21,36 +21,35 @@ export default class HomeHeaderComponent extends AppComponent {
                         <a class="text-light m-2" href="#">Devenir partenaire</a>
                     </div>
                     
-                    <img class="logo p-4" src="/app/uploads/2020/11/Logo-CC-e1605804875695.png">
+                    <img class="logo p-4" src="${getBaseSiteUrl()}/app/uploads/2020/11/Logo-CC-e1605804875695.png">
                     <div class="title">
-                        <h1 class="text-light">SOUTENEZ LES ARTISANS DE VOTRE REGION !</h1>
-                        <h2 class="text-light">Accédez à notre catalogue de produits artisanaux et locaux</h2>
+                        <h1 class="text-light text-center">SOUTENEZ LES ARTISANS DE VOTRE REGION !</h1>
+                        <h2 class="text-light text-center">Accédez à notre catalogue de produits artisanaux et locaux</h2>
                     </div>
                     
                     <div class="p-4 d-flex justify-content-center">
                     <app-wrapper class="app-wrapper">
                     <form >
-                        <div class="form-search p-4">
-                            <div class="input-group mb-3">
+                        <div class="form-search">
+                            <div class="mb-3 d-flex justify-content-center">
                                 
                                 <div class="mb-1">
-                                    <label for="searchBox" class="form-label text-black-50">Spécialité</label>
+                                    <label for="search-box-1" class="text-black-50">Spécialité</label>
                                     <span>
-                                    <input class="form-control" name="searchBox" type="text" placeholder="Quel corps de métier?">
+                                    <input class="form-control" name="search-box-1" type="text" placeholder="Quel corps de métier?">
                                     </span>
                                 </div>
                                 <div class="mb-1">
-                                    <label for="searchBox" class="form-label text-black-50">Ville</label>
+                                    <label for="search-box-2" class="text-black-50">Ville</label>
                                     <span>
-                                    <input class="form-control" id="ville" name="searchBox" type="text" placeholder="Où?">
+                                    <input class="form-control" id="city" name="search-box-2" type="text" placeholder="Où?">
                                     </span>
                                 </div>
-                                
-                                <span>
+                                <div>
                                     <button class="btn gradient gradient-primary" type="button">
                                         <app-icon icon="search" > </app-icon>
                                     </button>
-                                </span>
+                                </div>
                             </div>
                             
                         </div>
