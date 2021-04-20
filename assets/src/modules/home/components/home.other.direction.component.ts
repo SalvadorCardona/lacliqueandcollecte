ffa2265a0c95aad9a8ad277ad4e8c8bcecd2c365
suleymanthr <suleymanthr@outlook.fr>
2021-04-20 15:39:55 +0200

    Feat - other direction component

--- assets/src/modules/home/components/home.other.direction.component.ts
+++ assets/src/modules/home/components/home.other.direction.component.ts
@@ -1,5 +1,6 @@
 import {AppComponent} from 'App/types/custom.element';
 import {html, TemplateResult} from 'lit-element';
+import {getBaseSiteUrl} from "App/shared/helper";
 
 export default class HomeOtherDirectionComponent extends AppComponent {
 
@@ -9,7 +10,25 @@ export default class HomeOtherDirectionComponent extends AppComponent {
 
     public render(): TemplateResult {
         return html`
-            <div>Hello Home app-home-other-direction</div>
+            <div class="container-fluid">
+                <div class="row">
+                    <div class="with-background col-md-4" style="background-image: url('${getBaseSiteUrl()}/app/uploads/2021/04/picture-view-1.jpg')">image-1</div>
+                    <div class="without-background col-md-4">
+                        <div class="h-50"></div>
+                        <div class="h-50"></div>
+                    </div>
+                    <div class="with-background col-md-4" style="background-image: url('${getBaseSiteUrl()}/app/uploads/2021/04/picture-view-2.jpg')">image-2</div>
+                    <div class="without-background col-md-4">
+                        <div class="h-50"></div>
+                        <div class="h-50"></div>
+                    </div>
+                    <div class="with-background col-md-4" style="background-image: url('${getBaseSiteUrl()}/app/uploads/2021/04/picture-view-3.jpg')">image-3</div>
+                    <div class="without-background col-md-4">
+                        <div class="h-50"></div>
+                        <div class="h-50"></div>
+                    </div>
+                </div>
+            </div>
         `;
     }
 }
