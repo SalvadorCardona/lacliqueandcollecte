2fded20eb08d1e782e72e0709f2e8bfb39d358c2
suleymanthr <suleymanthr@outlook.fr>
2021-04-21 14:41:59 +0200

    fix - other direction component

--- assets/src/modules/home/components/home.other.direction.component.ts
+++ assets/src/modules/home/components/home.other.direction.component.ts
@@ -1,6 +1,10 @@
 import {AppComponent} from 'App/types/custom.element';
 import {html, TemplateResult} from 'lit-element';
 import {getBaseSiteUrl} from "App/shared/helper";
+import image1 from "Media/Image1.jpg";
+import image2 from "Media/Image2.jpg";
+import image3 from "Media/Image3.jpg";
+
 
 export default class HomeOtherDirectionComponent extends AppComponent {
 
@@ -12,20 +16,29 @@ export default class HomeOtherDirectionComponent extends AppComponent {
         return html`
             <div class="container-fluid">
                 <div class="row">
-                    <div class="with-background col-md-4" style="background-image: url('${getBaseSiteUrl()}/app/uploads/2021/04/picture-view-1.jpg')">image-1</div>
-                    <div class="without-background col-md-4">
-                        <div class="h-50"></div>
-                        <div class="h-50"></div>
+                    <div class="with-background col-md-4" style="background-image: url('${image2}')"></div>
+                    <div class="without-background col-md-4" >
+                        <div class="h-50"><h4 class="fw-normal text-dark">Zartisana</h4></div>
+                        <div class="h-50">
+                            <span>Qui sommes-nous?</span>
+                            <app-button icon="" label="Notre histoire" type="primary"> </app-button>
+                            
+                        </div>
                     </div>
-                    <div class="with-background col-md-4" style="background-image: url('${getBaseSiteUrl()}/app/uploads/2021/04/picture-view-2.jpg')">image-2</div>
+                    <div class="with-background col-md-4" style="background-image: url('${image3}')"></div>
                     <div class="without-background col-md-4">
-                        <div class="h-50"></div>
-                        <div class="h-50"></div>
+                        <div class="h-50"><h4 class="fw-normal text-dark">Vous êtes artisan ?</h4></div>
+                        <div class="h-50">
+                            <span>Devenez partenaire de notre association.</span>
+                            <app-button icon="" label="Contactez-nous" type="primary"> </app-button>
+                        </div>
                     </div>
-                    <div class="with-background col-md-4" style="background-image: url('${getBaseSiteUrl()}/app/uploads/2021/04/picture-view-3.jpg')">image-3</div>
+                    <div class="with-background col-md-4" style="background-image: url('${image1}')"></div>
                     <div class="without-background col-md-4">
-                        <div class="h-50"></div>
-                        <div class="h-50"></div>
+                        <div class="h-50"><h4 id="title-3" class="fw-normal text-dark">Découvrez les artisans et produits Zartizana.</h4></div>
+                        <div class="h-50">
+                            <app-button icon="" label="Le shop ici" type="primary"> </app-button>
+                        </div>
                     </div>
                 </div>
             </div>
