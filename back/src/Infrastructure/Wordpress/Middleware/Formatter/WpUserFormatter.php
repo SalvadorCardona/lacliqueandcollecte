05f389b63ad267aad97a4d74f9bd23cd62bf5f32
Camille <camille.boutin1@gmail.com>
2021-05-26 10:40:35 +0200

    Feat : update MiddlewareUser

--- back/src/Infrastructure/Wordpress/Middleware/Formatter/WpUserFormatter.php
+++ back/src/Infrastructure/Wordpress/Middleware/Formatter/WpUserFormatter.php
@@ -11,7 +11,6 @@ class WpUserFormatter extends Formatter
 {
     public function __construct(
         private WordpressMiddleware $wordpressMiddleware,
-        private MiddlewareUser $user
     ) {
     }
 
@@ -21,11 +20,7 @@ class WpUserFormatter extends Formatter
      **/
     public function format($data): mixed
     {
-        if (!$data instanceof WP_User) {
-            return [];
-        }
-        return ['ID' => $data->ID, 'displayName' => $data->display_name, 'niceName' => $data->user_nicename, 'email' => $data->user_email, 'roles' => $data->roles];
-
-//        return $this->user->initUser($data->ID, $data->user_lastname, $data->user_nicename, $data->user_email, $data->roles);
+        $user = new MiddlewareUser();
+        return $user->initUser($data->ID, $data->user_lastname, $data->user_nicename, $data->user_email, $data->roles);
     }
 }
