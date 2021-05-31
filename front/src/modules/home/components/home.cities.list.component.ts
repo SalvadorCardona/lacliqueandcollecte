b3a8acadf1d655e429a44d0faa3f56dd76f4f33f
romain longeron <romain.longeron@gmail.com>
2021-05-31 12:08:05 +0200

    change images cities and style card cities

--- front/src/modules/home/components/home.cities.list.component.ts
+++ front/src/modules/home/components/home.cities.list.component.ts
@@ -1,7 +1,12 @@
 import {AppComponent} from 'App/core/custom.element';
 import {html, TemplateResult} from 'lit-element';
+import lyon from "Media/home/cities/lyon.jpg";
+import paris from "Media/home/cities/paris.jpg";
+import nancy from "Media/home/cities/nancy.jpg";
 
 export default class HomeCitiesListComponent extends AppComponent {
+
+    private baseUrl: string = "city";
 
     public static getComponentName(): string {
         return 'app-home-cities-list';
@@ -9,40 +14,57 @@ export default class HomeCitiesListComponent extends AppComponent {
 
     private fakeData = [
         {
-            img: 'test',
-            nameCiTy: 'Nancy',
-            url: 'Nancyfake',
+            img: nancy,
+            labelCity: 'Nancy',
+            name: 'nancy',
         },
         {
-            img: 'test',
-            nameCiTy: 'Paris',
-            url: 'Nancyfake',
+            img: paris,
+            labelCity: 'Paris',
+            name: 'paris',
         },
         {
-            img: 'test',
-            nameCiTy: 'Lyon',
-            url: 'Nancyfake',
+            img: lyon,
+            labelCity: 'Lyon',
+            name: 'lyon',
         }
     ];
+
+    private getUrl(name: string): string
+    {
+        return this.baseUrl + '/' + name;
+    }
 
     public render(): TemplateResult {
         return html`
             <div class="container
-                mt-5
                 p-5
                 text-center">
                 <h2 class="title-border">Nos villes</h2>
-                
-                ${this.fakeData.map(data => this.renderCities(data))}
+                <div class="row">
+                    ${this.fakeData.map(data => this.renderCities(data))}
+                </div>
             </div>
         `;
     }
 
     public renderCities(data): TemplateResult {
         return html `
-            <div class="col-md-4" style="background-image: url('mon-image.jpg'); background-size: cover">
-                ${data.nameCiTy}
-            </div>
+            <div class="col-md-4 text-white mt-3">
+                <a class="btn
+                    btn-primary
+                    display-block
+                    py-5
+                    d-block
+                    border-radius
+                    background-overlay
+                    overflow-hidden"
+                   role="button"
+                   href=${this.getUrl(data.name)}
+                   style="background-image: url('${data.img}'); background-size: cover">
+                    <span class="fs-5 my-5 d-inline-block">${data.labelCity}</span>
+                </a>
+                </div>
         `;
     }
 }
