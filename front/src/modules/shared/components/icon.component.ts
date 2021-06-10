3b269a6c1cb7394a9533b2ea40dd5bcbfba3f5c3
Camille <camille.boutin1@gmail.com>
2021-06-10 14:13:25 +0200

    refacto icons

--- front/src/modules/shared/components/icon.component.ts
+++ front/src/modules/shared/components/icon.component.ts
@@ -3,6 +3,7 @@ import {icons} from 'App/core/icons'
 import {html, property, TemplateResult} from 'lit-element';
 import {unsafeHTML} from 'lit-html/directives/unsafe-html';
 import {Color} from "App/enum/color.enum";
+import {Icon} from "App/enum/icon.enum";
 
 export default class IconComponent extends AppComponent {
 
@@ -10,31 +11,35 @@ export default class IconComponent extends AppComponent {
         return 'app-icon';
     }
 
-    @property({type: String})
-    private icon: string;
+    @property({type: Icon})
+    private icon: Icon;
 
     @property({type: String})
-    private color: Color;
+    private color?: Color;
 
     @property({type: String})
-    private height: string;
+    private height?: string;
 
     @property({type: String})
-    private width: string;
+    private width?: string;
 
     public connectedCallback(): void {
-        this.classList.add(this.color);
+        if(this.color) {
+            this.classList.add(this.color)
+        }
         super.connectedCallback();
     }
 
     public firstUpdated(): void {
         const svg = this.querySelector('svg');
-
         if (this.width) {
             svg.style.width = this.width;
         }
         if (this.height) {
             svg.style.height = this.height;
+        }
+        if (this.color){
+            svg.style.fill = this.color;
         }
     }
 
@@ -42,3 +47,4 @@ export default class IconComponent extends AppComponent {
         return html`<i>${unsafeHTML(icons[this.icon] ? icons[this.icon] : '')}</i>`;
     }
 }
+
