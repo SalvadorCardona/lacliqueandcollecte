8326d70dc329b227d630420e8ff7cb81e78bfb70
Salvador <hebustha@gmail.com>
2021-06-16 14:18:10 +0200

    Clean Code

--- back/src/Infrastructure/PartnerRequest/Action/AddPartnerRequestPostType.php
+++ back/src/Infrastructure/PartnerRequest/Action/AddPartnerRequestPostType.php
@@ -7,8 +7,9 @@ namespace App\Infrastructure\PartnerRequest\Action;
 use App\Infrastructure\PartnerRequest\Entity\PartnerRequest;
 use App\Infrastructure\Wordpress\Action\ActionInterface;
 use App\Infrastructure\Wordpress\Middleware\WordpressMiddleware;
-use WordPlate\Acf\Fields\Tab;
+use WordPlate\Acf\Fields\Email;
 use WordPlate\Acf\Fields\Text;
+use WordPlate\Acf\Fields\Textarea;
 use WordPlate\Acf\Location;
 
 class AddPartnerRequestPostType implements ActionInterface
@@ -23,26 +24,24 @@ class AddPartnerRequestPostType implements ActionInterface
         $this->wordpressMiddleware->registerPostType(PartnerRequest::POST_TYPE_NAME, [
             'label' => $this->wordpressMiddleware->trans('partner.request'),
             'public' => true,
-            'menu_position' => 3,
-            'menu_icon' => 'dashicons-building',
-            'supports' => [],
+            'menu_icon' => 'dashicons-admin-users',
+            'supports' => ['custom-fields'],
             'show_in_rest' => false,
             'has_archive' => false,
         ]);
 
         $this->wordpressMiddleware->registerExtendedFieldGroup([
-            'title' => $this->wordpressMiddleware->trans('Partner Request'),
+            'title' => $this->wordpressMiddleware->trans('partner.request'),
             'location' => [
                 Location::if('post_type', PartnerRequest::POST_TYPE_NAME)
             ],
             'fields' => [
-                Tab::make($this->wordpressMiddleware->trans('Partner Request'))->placement('left'),
                 Text::make($this->wordpressMiddleware->trans('firstName'), 'firstName'),
                 Text::make($this->wordpressMiddleware->trans('lastName'), 'lastName'),
                 Text::make($this->wordpressMiddleware->trans('phone'), 'phone'),
-                Text::make($this->wordpressMiddleware->trans('email'), 'email'),
+                Email::make($this->wordpressMiddleware->trans('email'), 'email'),
                 Text::make($this->wordpressMiddleware->trans('siretNumber'), 'siretNumber'),
-                Text::make($this->wordpressMiddleware->trans('description'), 'description'),
+	            Textarea::make($this->wordpressMiddleware->trans('description'), 'description'),
             ]
         ]);
     }
