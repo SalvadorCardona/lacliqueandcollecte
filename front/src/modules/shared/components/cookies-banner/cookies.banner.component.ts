91b439cce0bda783038ffbd25d4e6c4dd80ef037
Salvador <hebustha@gmail.com>
2021-06-21 17:33:21 +0200

    Clean Clode

--- front/src/modules/shared/components/cookies-banner/cookies.banner.component.ts
+++ front/src/modules/shared/components/cookies-banner/cookies.banner.component.ts
@@ -4,12 +4,77 @@ import cookie from "Media/shared/cookie.svg";
 import ButtonComponent from "App/modules/shared/components/button.component";
 import {Color} from "App/enum/color.enum";
 
+interface CookieAcceptance {
+   hasAccepted: boolean;
+   dateAccepted: Date;
+}
+
 export default class CookiesBannerComponent extends AppComponent {
 
     public static getComponentName(): string {
         return 'app-cookies-banner';
     }
 
+    private maxDayCookie: number = 30;
+
+    public connectedCallback(): void {
+
+        const cookieAcceptance = this.getCookieAcceptance();
+        console.log(cookieAcceptance)
+        if (cookieAcceptance) {
+            const today = new Date();
+            const deltaTime = today.getTime() - cookieAcceptance.dateAccepted.getTime();
+            console.log(deltaTime)
+            console.log(deltaTime / (24*3600))
+            if (deltaTime / (24*3600) < this.maxDayCookie) {
+                this.hide();
+
+                return;
+            }
+        }
+
+        this.show();
+
+        super.connectedCallback();
+    }
+
+    private hide(): void {
+        this.hidden = true;
+    }
+
+    private show(): void {
+        this.hidden = false;
+    }
+
+    private getCookieAcceptance(): CookieAcceptance|null {
+        const dataCookieAcceptance =  JSON.parse(localStorage.getItem(CookiesBannerComponent.getComponentName()));
+
+        if (!dataCookieAcceptance) return null;
+
+        return {
+            dateAccepted: new Date(dataCookieAcceptance.dateAccepted),
+            hasAccepted: dataCookieAcceptance.hasAccepted
+        } as CookieAcceptance;
+    }
+
+    private setCookieAcceptance(hasAccepted: boolean): void {
+        const cookieAcceptance = {
+            dateAccepted: new Date(),
+            hasAccepted: hasAccepted
+        } as CookieAcceptance;
+
+        localStorage.setItem(CookiesBannerComponent.getComponentName(), JSON.stringify(cookieAcceptance))
+    }
+
+    private onAccepted(): void {
+        this.setCookieAcceptance(true);
+        this.hide();
+    }
+
+    private onRefused(): void {
+        this.setCookieAcceptance(false);
+        this.hide();
+    }
 
     public render(): TemplateResult {
         return html`
@@ -42,13 +107,16 @@ export default class CookiesBannerComponent extends AppComponent {
                         ">
                         ${this.createElement(ButtonComponent, {
                             label: this.trans("cookies.banner.refuse"),
+                            $click: () => this.onRefused()
                         })}
                         ${this.createElement(ButtonComponent, {
                             label: this.trans("cookies.banner.accept"),
-                            type: Color.DARK
+                            type: Color.DARK,
+                            $click: () => this.onAccepted()
                         })}
                     </div>
             </div>
         `;
     }
+
 }
